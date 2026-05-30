33df9d42f2f4cbb875bfc6b8a851de404475cdb7
Buffden <buffdenslays@gmail.com>
2026-05-29 21:54:14 -0500

    chat pannel template

---
+++ frontend/src/app/features/dashboard/components/chat-panel/chat-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-panel',
+  imports: [],
+  templateUrl: './chat-panel.component.html',
+  styleUrl: './chat-panel.component.scss'
+})
+export class ChatPanelComponent {
+
+}
